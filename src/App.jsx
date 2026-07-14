f1b4d27f66a7276cdcb1075c1009a573c86b70bc
JosueS-creator <ventaspublicalle@gmail.com>
2026-07-14 17:08:46 -0600

    Add routes for AdminActualizarPassword and Comentarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing'
 import AdminLogin from './pages/AdminLogin'
+import AdminActualizarPassword from './pages/AdminActualizarPassword'
 import AdminLayout from './pages/admin/AdminLayout'
 import Dashboard from './pages/admin/Dashboard'
 import Estaciones from './pages/admin/Estaciones'
 import Clientes from './pages/admin/Clientes'
 import Facturas from './pages/admin/Facturas'
 import Referidos from './pages/admin/Referidos'
+import Comentarios from './pages/admin/Comentarios'
 import Reportes from './pages/admin/Reportes'
 import ProtectedRoute from './components/ProtectedRoute'
 
@@ -16,6 +18,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/admin/login" element={<AdminLogin />} />
+        <Route path="/admin/actualizar-password" element={<AdminActualizarPassword />} />
         <Route
           path="/admin"
           element={
@@ -29,6 +32,7 @@ export default function App() {
           <Route path="clientes" element={<Clientes />} />
           <Route path="facturas" element={<Facturas />} />
           <Route path="referidos" element={<Referidos />} />
+          <Route path="comentarios" element={<Comentarios />} />
           <Route path="reportes" element={<Reportes />} />
         </Route>
       </Routes>
